4f416442d5296353ae6409c4864372d639cd6a85
rideLite <tagir-sharafutdinov@rambler.ru>
2024-11-03 18:27:24 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!11111111");
+        System.out.println("Hello world!11111111");
+        System.out.println("Hello world!11111111");
+        System.out.println("Hello world!11111111");
+        System.out.println("Hello world!11111111");
+
     }
 }
